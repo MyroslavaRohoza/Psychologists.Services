521c2c5e4e01e7b0430866c01c3d4c9fe5cdb41d
Myroslava Rogoza <myroslavarogoza@gmail.com>
2024-10-07 19:23:50 +0300

    homepage

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
 import css from "./Logo.module.css";
 
 export const Logo = () => {
-  return <div className={css.logo}><span className={css.logoAccent}>psychologists.</span>services</div>;
-}
+  return (
+    <div className={css.logo}>
+      <Link to="/">
+        <span className={css.logoAccent}>psychologists.</span>services
+      </Link>
+    </div>
+  );
+};
