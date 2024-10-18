603f2ecbac33ddd3790bd885b2e6f5bd01a62e13
Myroslava Rogoza <myroslavarogoza@gmail.com>
2024-10-18 14:41:50 +0300

    auth modal buttons

--- src/components/AuthMessage/AuthMessage.jsx
+++ src/components/AuthMessage/AuthMessage.jsx
@@ -1,15 +1,32 @@
-import FormDescription from "../FormDescription/FormDescription";
+import css from "./AuthMessage.module.css";
 import FormTitle from "../FormTitle/FormTitle";
 import GreenBtn from "../GreenBtn/GreenBtn";
 import LoginBtn from "../LoginBtn/LoginBtn";
+import { useCallback } from "react";
+import { setModalName, setOpen } from "../../zustand/selectors";
 
 const AuthMessage = () => {
+
+  const onLoginBtnClick = useCallback(() => {
+   
+    setModalName("LogIn");   setOpen(true); 
+  })
+
+  const onRegistrationBtnClick = useCallback(() => {
+    setModalName("Register");setOpen(true);
+    
+  })
   return (
-    <div>
-      <FormTitle>This feature is for authorised users only</FormTitle>
-      <LoginBtn>Log in</LoginBtn>
-      <GreenBtn padding="14px 40px">Register</GreenBtn>
-    </div>
+    <>
+      <FormTitle>
+        This feature is
+        <br /> for authorised users only
+      </FormTitle>
+      <div className={css.btnContainer}>
+        <LoginBtn onClick={onLoginBtnClick}>Log in</LoginBtn>
+        <GreenBtn padding="14px 40px" onClick={onRegistrationBtnClick}>Register</GreenBtn>
+      </div>
+    </>
   );
 };
 
