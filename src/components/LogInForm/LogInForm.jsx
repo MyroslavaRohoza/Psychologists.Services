55d58e538f52c6dcff5b455e429f528e4b45ce53
Myroslava Rogoza <myroslavarogoza@gmail.com>
2024-11-10 19:18:48 +0200

    yup for login form

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -10,10 +10,32 @@ import FormTitle from "../FormTitle/FormTitle";
 import FormDescription from "../FormDescription/FormDescription";
 import { useBoundStore } from "../../zustand/store";
 import { getUserInfo } from "../../zustand/selectors";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  userName: yup.string().required("Name is required!"),
+  userEmail: yup
+    .string()
+    .email("Email is invalid")
+    .required("Email is required!"),
+  userPassword: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters long.")
+    .max(64, "Password must be between 8 and 64 characters."),
+});
 
 const LogInForm = () => {
   const isAuth = useBoundStore(getUserInfo).isSignedIn;
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onSubmit",
+  });
   const onSubmit = ({ userEmail, userPassword }) =>
     loginUser(userEmail, userPassword);
   return (
@@ -27,22 +49,34 @@ const LogInForm = () => {
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={css.formContainer}>
-          <InputField
-            type="email"
-            placeholder="Email"
-            inputName="userEmail"
-            register={register}
-            required
-          />
-          <InputField
-            type="password"
-            placeholder="Password"
-            inputName="userPassword"
-            register={register}
-            required
-          />
+          <label>
+            <InputField
+              type="email"
+              placeholder="Email"
+              inputName="userEmail"
+              register={register}
+              required
+            />
+            {errors.userEmail && (
+              <p className="errorMessage">{errors.userEmail.message}</p>
+            )}
+          </label>
+          <label>
+            <InputField
+              type="password"
+              placeholder="Password"
+              inputName="userPassword"
+              register={register}
+              required
+            />
+            {errors.userPassword && (
+              <p className="errorMessage">{errors.userPassword.message}</p>
+            )}
+          </label>
         </div>
-        <GreenBtn height={"52px"} disabled={isAuth}>Sign Up</GreenBtn>
+        <GreenBtn height={"52px"} disabled={isAuth}>
+          Sign Up
+        </GreenBtn>
       </form>
     </div>
   );
