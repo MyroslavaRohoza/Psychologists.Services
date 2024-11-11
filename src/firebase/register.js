c9adb129a7f021aa18ac8de665197bf9b2cae872
Myroslava Rogoza <myroslavarogoza@gmail.com>
2024-11-11 15:38:37 +0200

    fixed registerUser

--- src/firebase/register.js
+++ src/firebase/register.js
@@ -2,8 +2,6 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   updateProfile,
-  signOut,
-  signInWithEmailAndPassword,
 } from "firebase/auth";
 import { loadUserInfo } from "../js/utilities";
 import { app } from "./firebase";
@@ -11,43 +9,21 @@ import { app } from "./firebase";
 const auth = getAuth(app);
 
 export const registerUser = async (name, email, password) => {
-  const user = await uploadDataToFirestore(auth, name, email, password);
-
-  if (user) {
-    try {
-      await updateProfile(user, { displayName: name });
-
-      await signOut(auth);
-
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log("User logged in:", userCredential.user);
-
-      loadUserInfo(userCredential.user);
-    } catch (error) {
-      console.error("Error updating user profile:", error);
-    }
-  }
-};
-
-export const uploadDataToFirestore = async (
-  auth,
-  displayName,
-  email,
-  password
-) => {
   try {
     const userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
-    return userCredential.user;
+
+    const user = userCredential.user;
+
+    await user.reload();
+
+    await updateProfile(user, { displayName: name });
+
+    loadUserInfo(userCredential.user);
   } catch (error) {
-    console.error("Error registering user:", error.message);
-    throw error;
+    console.error("Error updating user profile:", error);
   }
 };
